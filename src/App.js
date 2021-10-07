45d61ff34bb8df2529c10ef0fb61f0502ae7f557
marta <Vynnytska1989@gmail.com>
2021-10-07 14:14:05 +0300

    не працююча версія

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import { Statistics } from './components/Feedback';
+import { FeedbackOptions } from './components/Feedback';
+import { Section } from './components/Feedback';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  onLeaveFeedback = e => {
+    const currentEl = e.target.value;
+    this.setState(prev => {
+      return {
+        ...currentEl,
+        [currentEl]: prev[currentEl] + 1,
+      };
+    });
+  };
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const allReviews = this.countTotalFeedback();
+    const positivePersent = this.state.good;
+    return this.state.good
+      ? Math.ceil((positivePersent / allReviews) * 100)
+      : 0;
+  };
+  render() {
+    const { good, neutral, bad } = this.state;
+    const optionValue = ['good', 'neutral', 'bad'];
+    const {
+      onLeaveFeedback,
+      countTotalFeedback,
+      countPositiveFeedbackPercentage,
+    } = this;
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={optionValue}
+            onLeaveFeedback={onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      </div>
+    );
+  }
 }
 
 export default App;
